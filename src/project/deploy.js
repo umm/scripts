b147d796706e6168adad10dec3d6950589ad60e8
Tetsuya Mori <monry84@gmail.com>
2018-04-25 20:17:08 +0900

    :sparkles: Add script for deploy/remove projects

---
+++ src/project/deploy.js
@@ -0,0 +1,15 @@
+const util = require('util');
+const path = require('path');
+const info = require('../../lib/info');
+const synchronize = util.promisify(require('../../lib/synchronize'));
+
+module.exports = () => {
+  if (info.development_install) {
+    return;
+  }
+
+  synchronize(
+    path.join(info.package_path, 'Assets'),
+    path.join(info.base_path, 'Assets', 'Projects', info.name)
+  );
+};
