5bc7e207dc3f1c0701b62cf92b4db4e371541685
Tetsuya Mori <monry84@gmail.com>
2018-04-25 20:17:43 +0900

    :name_badge: Rename variable name

--- src/install.js
+++ src/install.js
@@ -1,14 +1,14 @@
 const util = require('util');
 const path = require('path');
 const info = require('../lib/info');
-const sync = util.promisify(require('../lib/synchronize'));
+const synchronize = util.promisify(require('../lib/synchronize'));
 
 module.exports = () => {
   if (info.development_install) {
     return;
   }
 
-  sync(
+  synchronize(
     path.join(info.package_path, 'Assets'),
     path.join(info.base_path, 'Assets', 'Modules', info.module_name)
   );
